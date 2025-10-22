0dd13c28469d6da9642af2cc4cdd9073af871448
Emmanuel Yankey <hiyankey@gmail.com>
2025-10-22 15:10:19 +0000

    Make interface responsive w/ easing

---
+++ app/(demos)/make-interface-responsive/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <button
+        className="h-7 rounded-md bg-white px-3 text-black transition-transform ease-out active:scale-[.9]"
+        type="button"
+      >
+        Click me!
+      </button>
+    </div>
+  );
+}
