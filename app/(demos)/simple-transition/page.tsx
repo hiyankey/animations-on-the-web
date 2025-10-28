4f06039c63fa336730bf24b75635e6d351784559
Emmanuel Yankey <hiyankey@gmail.com>
2025-10-28 14:30:20 +0000

    Simple transition

---
+++ app/(demos)/simple-transition/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return <div className="flex min-h-screen justify-center items-center">
+    <div className="hover:[&_div]:translate-y-[-20%] border"><div className="size-14 rounded-[50%] bg-amber-500 transition-transform [transition:300ms_ease]"/></div>
+  </div>
+}
